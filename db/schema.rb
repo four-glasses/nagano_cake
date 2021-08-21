ab47e11fe3413af043930696039c732bbd488572
EC2 Default User <ec2-user@ip-172-31-95-41.ec2.internal>
2021-08-21 05:32:03 +0000

    [Edit]deviceのmodel変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_032220) do
+ActiveRecord::Schema.define(version: 2021_08_21_052920) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 2021_08_18_032220) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
 end
