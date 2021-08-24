bbb2366771c0bbaef48178a1903bd3644325393a
EC2 Default User <ec2-user@ip-172-31-72-216.ec2.internal>
2021-08-24 01:35:27 +0000

    ヘッダー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,43 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def after_sign_in_path_for(resource)
+      if customer_signed_in?
+        customers_path(resource)
+      else
+        admin_top_path
+      end
+    end
+
+    def after_sign_out_path_for(resource)
+      root_path
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,
+  			 keys: [:first_name, :last_name, :kana_first_name, :kana_last_name,
+                :email, :postal_code, :residence, :phone_number])
+
+  		devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+
+    end
+
+  private
+
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def set_cart_item
+    @cart_item = CartItem.find(params[:id])
+  end
+
 end
