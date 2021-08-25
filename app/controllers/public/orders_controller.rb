c40f23c1e099c1d15e158f802a5af2e55e9217c2
EC2 Default User <ec2-user@ip-172-31-94-129.ec2.internal>
2021-08-25 07:36:19 +0000

    修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,44 +1,47 @@
-class OrdersController < ApplicationController
+class Public::OrdersController < ApplicationController
+
+
+before_action :params_check, only: [:index]
 
 def index
-  @customers = Customers.find(params[:id])
+  @customer = Customer.find(params[:id])
   @orders = @customers.orders
-  unless current_user.nil? || current_user.id == @customers.id
+  unless current_customer.nil? || current_customer.id == @customers.id
     flash[:warning] = "アクセス権がありません"
-    redirect_to orders_path(id: current_user.id)
+    redirect_to orders_path(id: current_customer.id)
   end
 end
 
 def comfirm
 @order = Order.find(params[:id])
 	# 他のuserのアクセス阻止
-		unless current_user.nil? || current_user.id == @order.user_id
+		unless current_customer.nil? || current_customer.id == @order.customer_id
     		flash[:warning] = "アクセス権がありません"
-			redirect_to orders_path(id: current_user.id)
+			redirect_to orders_path(id: current_customer.id)
 		end
 		@items = @order.ordered_items
 
 end
 
 def complete
-  cart_items = current_user.cart_items
+  cart_items = current_customer.cart_items
   cart_items.destroy_all
 end
 
 def create
 @order = Order.new(order_params)
-		@customers = current_user
-		@ads = @user.ship_to_addresses
+		@customers = current_customer
+		@ads = @customers.addresses
 			if params[:_add] == "usersAdd"
-				@order.ship_address = @customers.address
+				@order.address = @customers.address
 				@order.last_name = @customers.last_name
 				@order.first_name = @customers.first_name
 				@order.last_name_kana = @customers.last_name_kana
 				@order.first_name_kana = @customers.first_name_kana
-				@order.ship_postal_code = @customers.postal_code
+				@order.postal_code = @customers.postal_code
 			elsif params[:_add] == "shipAdds"
-				@ad = @ads.find(params[:ShipToAddress][:id])
-				@order.ship_address = @ad.address
+				@ad = @ads.find(params[:Address][:id])
+				@order.address = @ad.address
 				@order.last_name = @ad.last_name
 				@order.first_name = @ad.first_name
 				@order.last_name_kana = @ad.last_name_kana
@@ -46,7 +49,7 @@ def create
 				@order.ship_postal_code = @ad.postal_code
 			elsif params[:_add] == "newAdd"
 				@ad = ShipToAddress.new
-				@ad.user_id = @user.id
+				@ad.customer_id = @customer.id
 				@ad.address = params[:ship_to_address][:address]
 				@ad.last_name = params[:ship_to_address][:last_name]
 				@ad.first_name = params[:ship_to_address][:first_name]
@@ -68,26 +71,22 @@ end
 
 def show
   @order = Order.find(params[:id])
-	unless current_user.nil? || current_user.id == @order.user_id
+	unless current_customer.nil? || current_customer.id == @order.customer_id
     flash[:warning] = "アクセス権がありません"
-	  redirect_to orders_path(id: current_user.id)
+	  redirect_to orders_path(id: current_customer.id)
 	end
 end
 
 def new
-	@user = current_user
-	if @user.cart_items.blank?
+	@customer = current_customer
+	if @customer.cart_items.blank?
    	flash[:warning] = "カートが空です"
   	redirect_to cart_items_path
 	else
-  	@order = Order.new(user_id: @user.id)
-  	@ads = @user.ship_to_addresses
-  	@ship_to_address = ShipToAddress.new(user_id: @user.id)
+  	@order = Order.new(customer_id: @customer.id)
+  	@ads = @customer.addresses
+  	@address = ShipToAddress.new(customer_id: @customer.id)
 	end
 
 end
-
-
-
-
 end
