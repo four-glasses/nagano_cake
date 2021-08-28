3495aa7798be45be3b1e53161778cd31571f5733
andou-yuma <antengyuu099@gmail.com>
2021-08-28 08:17:46 +0000

    order,item変更

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!,only: [:create,:edit,:update,:index, :show, :new]
 
   def index
     @items = Item.page(params[:page]).per(10)
